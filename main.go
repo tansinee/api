3f7495e09aa03a79bd882c3cb9969ec4fc312d5d
pallat <yod.pallat@gmail.com>
2018-04-26 13:17:53 +0700

    echo with graceful shutdown

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/tylerb/graceful"
+)
+
+// Health ckecking Handler
+func well(c echo.Context) error {
+	return c.String(http.StatusOK, "ok")
+}
+
+func main() {
+	// Echo instance
+	e := echo.New()
+	// e.File("/favicon.ico", "images/favicon.ico")
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
+	// Routes
+	e.GET("/", well)
+
+	// Start server
+	e.Server.Addr = ":1323"
+
+	fmt.Printf("⇨ http server started on %s\n", e.Server.Addr)
+	// Serve it like a boss
+	graceful.ListenAndServe(e.Server, 5*time.Second)
+}
